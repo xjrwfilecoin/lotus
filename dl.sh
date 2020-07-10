194d61df7f10282c48ec38cdde451eb000b87acc
ocean <3408598@qq.com>
2020-07-10 17:43:50 +0800

    add sh

---
+++ dl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+git clone -b b_0.4.1 https://github.com/xjrwfilecoin/specs-storage.git /home/xjrw/specs-storage
+
+git clone -b b_0.4.1 https://github.com/xjrwfilecoin/sector-storage.git /home/xjrw/sector-storage
