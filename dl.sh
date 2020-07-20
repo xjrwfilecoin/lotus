add6865a952cf66b2bc7bfbd7d4ee2cac9fe0035
ocean <3408598@qq.com>
2020-07-20 22:20:04 +0800

    merge

---
+++ dl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+git clone -b butterfly-7.19.0 https://github.com/xjrwfilecoin/specs-storage.git /home/xjrw/specs-storage
+
+git clone -b butterfly-7.19.0 https://github.com/xjrwfilecoin/sector-storage.git /home/xjrw/sector-storage
