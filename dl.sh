30cdbf53c2868708c642583d04ed58bd7c1bffa9
ocean <3408598@qq.com>
2020-08-01 16:14:34 +0800

    merge

---
+++ dl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+git clone -b calibration https://github.com/xjrwfilecoin/specs-storage.git /home/xjrw/specs-storage
+
+git clone -b calibration-8.1.0 https://github.com/xjrwfilecoin/sector-storage.git /home/xjrw/sector-storage
+
+git clone -b calibration-8.1.0 https://github.com/xjrwfilecoin/storage-fsm.git /home/xjrw/storage-fsm
