bd11a43ed0340e37c44218330c7d218b9bb36535
ocean <3408598@qq.com>
2020-08-20 09:07:14 +0800

    merge

--- extern/sector-storage/manager.go
+++ extern/sector-storage/manager.go
@@ -65,6 +65,7 @@ type WorkerID uint64
 type Manager struct {
 	scfg *ffiwrapper.Config
 
+	mapReal map[abi.SectorID]struct{}
 	ls         stores.LocalStorage
 	storage    *stores.Remote
 	localStore *stores.Local
@@ -111,11 +112,13 @@ func New(ctx context.Context, ls stores.LocalStorage, si stores.SectorIndex, cfg
 		remoteHnd:  &stores.FetchHandler{Local: lstor},
 		index:      si,
 
+		mapReal: make(map[abi.SectorID]struct{}),
 		sched: newScheduler(cfg.SealProofType),
 
 		Prover: prover,
 	}
 
+	initState()
 	go m.sched.runSched()
 
 	localTasks := []sealtasks.TaskType{
@@ -282,11 +285,11 @@ func (m *Manager) ReadPiece(ctx context.Context, sink io.Writer, sector abi.Sect
 }
 
 func (m *Manager) NewSector(ctx context.Context, sector abi.SectorID) error {
-	log.Warnf("stub NewSector")
+	log.Warnf("stub NewSector %v", sector)
 	return nil
 }
 
-func (m *Manager) AddPiece(ctx context.Context, sector abi.SectorID, existingPieces []abi.UnpaddedPieceSize, sz abi.UnpaddedPieceSize, r io.Reader) (abi.PieceInfo, error) {
+func (m *Manager) oldAddPiece(ctx context.Context, sector abi.SectorID, existingPieces []abi.UnpaddedPieceSize, sz abi.UnpaddedPieceSize, r io.Reader) (abi.PieceInfo, error) {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
@@ -315,7 +318,7 @@ func (m *Manager) AddPiece(ctx context.Context, sector abi.SectorID, existingPie
 	return out, err
 }
 
-func (m *Manager) SealPreCommit1(ctx context.Context, sector abi.SectorID, ticket abi.SealRandomness, pieces []abi.PieceInfo) (out storage.PreCommit1Out, err error) {
+func (m *Manager) oldSealPreCommit1(ctx context.Context, sector abi.SectorID, ticket abi.SealRandomness, pieces []abi.PieceInfo) (out storage.PreCommit1Out, err error) {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
@@ -339,7 +342,7 @@ func (m *Manager) SealPreCommit1(ctx context.Context, sector abi.SectorID, ticke
 	return out, err
 }
 
-func (m *Manager) SealPreCommit2(ctx context.Context, sector abi.SectorID, phase1Out storage.PreCommit1Out) (out storage.SectorCids, err error) {
+func (m *Manager) oldSealPreCommit2(ctx context.Context, sector abi.SectorID, phase1Out storage.PreCommit1Out) (out storage.SectorCids, err error) {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
@@ -360,7 +363,7 @@ func (m *Manager) SealPreCommit2(ctx context.Context, sector abi.SectorID, phase
 	return out, err
 }
 
-func (m *Manager) SealCommit1(ctx context.Context, sector abi.SectorID, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (out storage.Commit1Out, err error) {
+func (m *Manager) oldSealCommit1(ctx context.Context, sector abi.SectorID, ticket abi.SealRandomness, seed abi.InteractiveSealRandomness, pieces []abi.PieceInfo, cids storage.SectorCids) (out storage.Commit1Out, err error) {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
@@ -384,7 +387,7 @@ func (m *Manager) SealCommit1(ctx context.Context, sector abi.SectorID, ticket a
 	return out, err
 }
 
-func (m *Manager) SealCommit2(ctx context.Context, sector abi.SectorID, phase1Out storage.Commit1Out) (out storage.Proof, err error) {
+func (m *Manager) oldSealCommit2(ctx context.Context, sector abi.SectorID, phase1Out storage.Commit1Out) (out storage.Proof, err error) {
 	selector := newTaskSelector()
 
 	err = m.sched.Schedule(ctx, sector, sealtasks.TTCommit2, selector, schedNop, func(ctx context.Context, w Worker) error {
@@ -399,7 +402,7 @@ func (m *Manager) SealCommit2(ctx context.Context, sector abi.SectorID, phase1Ou
 	return out, err
 }
 
-func (m *Manager) FinalizeSector(ctx context.Context, sector abi.SectorID, keepUnsealed []storage.Range) error {
+func (m *Manager) oldFinalizeSector(ctx context.Context, sector abi.SectorID, keepUnsealed []storage.Range) error {
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
---
+++ extern/sector-storage/xjrw_state.go
@@ -0,0 +1,195 @@
+package sectorstorage
+
+import (
+	"database/sql"
+	"github.com/filecoin-project/lotus/extern/sector-storage/sealtasks"
+	_ "github.com/mattn/go-sqlite3"
+	"os"
+	"os/exec"
+	"sync"
+	"time"
+)
+
+type SectorState struct {
+	Start  string
+	End    string
+	Worker string
+}
+
+var smu sync.Mutex
+var db *sql.DB = nil
+var state = map[string]map[sealtasks.TaskType]*SectorState{}
+
+func ShellExecute(cmdStr string) error {
+	cmd := exec.Command("/bin/bash", "-c", cmdStr, "|sh")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	err := cmd.Run()
+	log.Infof("ShellExecute %s : %v", cmdStr, err)
+	return err
+}
+
+func initState() {
+	var err error
+	db, err = sql.Open("sqlite3", "./sector.db")
+	if err != nil {
+		panic(err)
+	}
+
+	sqlStmt := `
+	create table if not exists sector (id VARCHAR(64) not null primary key, addpiecestart text,addpieceend text,addpiecehost text,p1start text,p1end text,p1host text,p2start text,p2end text,p2host text, c1start text,c1end text,c1host text, c2start text,c2end text,c2host text);
+	`
+	_, err = db.Exec(sqlStmt)
+	if err != nil {
+		panic(err)
+		return
+	}
+}
+
+func startSector(sector, wk string, tk sealtasks.TaskType) {
+	smu.Lock()
+	defer smu.Unlock()
+
+	_, ok := state[sector]
+	if !ok {
+		state[sector] = map[sealtasks.TaskType]*SectorState{}
+	}
+	_, ok = state[sector][tk]
+	if !ok {
+		state[sector][tk] = &SectorState{
+			Worker: wk,
+		}
+	}
+	state[sector][tk].Start = time.Now().Format(time.RFC3339)
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Errorf("sqllite begin %v", err)
+	}
+
+	pre := ""
+	if tk == sealtasks.TTAddPiece {
+		pre = "replace into sector(id, addpiecestart,addpiecehost) values(?, ?, ?)"
+	} else if tk == sealtasks.TTPreCommit1 {
+		pre = "update sector set p1start = ?, p1host = ?  where id = ?"
+	} else if tk == sealtasks.TTPreCommit2 {
+		pre = "update sector set p2start = ? ,p2host = ? where id = ?"
+	} else if tk == sealtasks.TTCommit1 {
+		pre = "update sector set c1start = ? ,c1host = ? where id = ?"
+	} else if tk == sealtasks.TTCommit2 {
+		pre = "update sector set c2start = ? ,c2host = ? where id = ?"
+	} else {
+		log.Errorf("not support type")
+		return
+	}
+
+	stmt, err := tx.Prepare(pre)
+	if err != nil {
+		log.Errorf("sqllite prepare %v", err)
+	}
+	defer stmt.Close()
+	if tk == sealtasks.TTAddPiece {
+		_, err = stmt.Exec(sector, state[sector][tk].Start, wk)
+	} else {
+		_, err = stmt.Exec(state[sector][tk].Start, wk, sector)
+	}
+
+	if err != nil {
+		log.Errorf("sqllite exec %v", err)
+	}
+	tx.Commit()
+}
+
+func endSector(sector, wk string, tk sealtasks.TaskType) {
+	smu.Lock()
+	defer smu.Unlock()
+
+	_, ok := state[sector]
+	if !ok {
+		state[sector] = map[sealtasks.TaskType]*SectorState{}
+	}
+	_, ok = state[sector][tk]
+	if !ok {
+		state[sector][tk] = &SectorState{
+			Worker: wk,
+		}
+	}
+	state[sector][tk].End = time.Now().Format(time.RFC3339)
+
+	tx, err := db.Begin()
+	if err != nil {
+		log.Errorf("sqllite begin %v", err)
+	}
+
+	pre := ""
+	if tk == sealtasks.TTAddPiece {
+		pre = "update sector set addpieceend = ? where id = ?"
+	} else if tk == sealtasks.TTPreCommit1 {
+		pre = "update sector set p1end = ? where id = ?"
+	} else if tk == sealtasks.TTPreCommit2 {
+		pre = "update sector set p2end = ? where id = ?"
+	} else if tk == sealtasks.TTCommit1 {
+		pre = "update sector set c1end = ? where id = ?"
+	} else if tk == sealtasks.TTCommit2 {
+		pre = "update sector set c2end = ? where id = ?"
+	} else {
+		log.Errorf("not support type")
+		return
+	}
+
+	stmt, err := tx.Prepare(pre)
+	if err != nil {
+		log.Errorf("sqllite prepare %v", err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(state[sector][tk].End, sector)
+	if err != nil {
+		log.Errorf("sqllite exec %v", err)
+	}
+	tx.Commit()
+}
+
+func findSector(sector string, tk sealtasks.TaskType) string {
+	smu.Lock()
+	defer smu.Unlock()
+
+	_, ok := state[sector][tk]
+	if !ok {
+		que := ""
+		if tk == sealtasks.TTAddPiece {
+			que = "select addpiecehost from sector where id = "
+		} else if tk == sealtasks.TTPreCommit1 {
+			que = "select p1host from sector where id = "
+		} else if tk == sealtasks.TTPreCommit2 {
+			que = "select p2host from sector where id = "
+		} else if tk == sealtasks.TTCommit1 {
+			que = "select c1host from sector where id = "
+		} else if tk == sealtasks.TTCommit2 {
+			que = "select c2host from sector where id = "
+		} else {
+			log.Errorf("not support type")
+			return ""
+		}
+		que += "'" + sector + "'"
+
+		rows, err := db.Query(que)
+		if err != nil {
+			log.Errorf("sqllite query %v %v ", err, que)
+		}
+
+		defer rows.Close()
+		for rows.Next() {
+			var host string
+			err = rows.Scan(&host)
+			if err != nil {
+				log.Errorf("sqllite scan %v %v", err, que)
+			}
+			return host
+		}
+		return ""
+	}
+
+	return state[sector][tk].Worker
+}
