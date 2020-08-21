ebe4c7f7614d20a1e9c894e70f073744819a9ee1
zhu <pluto.shu@gmail.com>
2020-08-21 15:28:58 +0800

    添加脚本

---
+++ fast_make.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+if [ $# != 1 ] ; then
+	echo "usage make_lotus.sh local/remote/raw"
+fi
+trap 'rm -f "$TMPFILE"' EXIT
+#生成临时目录
+#TMPFILE=$(mktemp) || exit 1
+##git clone 相关的库
+#cd $TMPFILE
+##rust-filecoin-proofs-api
+#git clone https://github.com/plotozhu/rust-filecoin-proofs-api
+#cd rust-filecoin-proofs-api
+#git checkout local
+#cd ..
+## rust-files
+#mkdir   ./rust-file-local
+#cd rust-file-local
+rm -fr ./extern/filecoin-ffi
+mkdir -p ./extern/filecoin-ffi
+
+if [ $1 == "local" ];then
+  echo "using local ffi"
+	cp -fr  extern/xjrw-ffi/* extern/filecoin-ffi/
+elif [ $1 == "remote" ]; then
+  echo "using remote ffi "
+	cp -fr extern/DBC-filecoin-ffi/* extern/filecoin-ffi/
+elif [ $1 == "origin"]; then
+  echo "use origin"
+else
+	echo "param should be remote or local"
+	exit
+fi
+
+(env 'RUSTFLAGS=-C target-cpu=native -g' FFI_BUILD_FROM_SOURCE=1 make clean all lotus-bench)
+
+mkdir -p ~/lotus-$1
+cp lotus lotus-* ~/lotus-$1
+
