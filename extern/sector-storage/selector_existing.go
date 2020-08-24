f2cf2548f2dc90b53f6138df3f46f853e40aba5e
ocean <3408598@qq.com>
2020-08-25 06:29:29 +0800

    add error

--- extern/sector-storage/selector_existing.go
+++ extern/sector-storage/selector_existing.go
@@ -45,7 +45,7 @@ func (s *existingSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt 
 		pwk := findSector(stores.SectorName(s.sector), sealtasks.TTPreCommit2)
 		log.Infof("xjrw %v task = %s  pwk = %s hostname = %s", s.sector, task, pwk, inf.Hostname)
 		if pwk == "" {
-			return false, nil
+			return false, xerrors.Errorf("%v not exist", s.sector)
 		}
 		if pwk != inf.Hostname {
 			return false, nil
