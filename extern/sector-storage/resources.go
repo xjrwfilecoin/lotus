da170954a912c0728cadcbfe441ae457bcb95e1c
ocean <3408598@qq.com>
2020-08-25 14:56:00 +0800

    limit 7P1

--- extern/sector-storage/resources.go
+++ extern/sector-storage/resources.go
@@ -73,7 +73,7 @@ var ResourceTable = map[sealtasks.TaskType]map[abi.RegisteredSealProof]Resources
 			BaseMinMemory: 10 << 20,
 		},
 		abi.RegisteredSealProof_StackedDrg32GiBV1: Resources{
-			MaxMemory: 75 << 30,
+			MaxMemory: 64 << 30,
 			MinMemory: 56 << 30,
 
 			Threads: 1,
