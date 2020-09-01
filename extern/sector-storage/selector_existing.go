6d9fd3d7db86274268ae0c6f62e61113c7f0d643
ocean <3408598@qq.com>
2020-09-01 17:43:31 +0800

    merge

--- extern/sector-storage/selector_existing.go
+++ extern/sector-storage/selector_existing.go
@@ -41,17 +41,30 @@ func (s *existingSelector) Ok(ctx context.Context, task sealtasks.TaskType, spt 
 		return false, xerrors.Errorf("getting worker info: %w", err)
 	}
 
-	if task == sealtasks.TTPreCommit2 {
+	if group, exist := groupState[inf.Hostname]; task == sealtasks.TTPreCommit2 {
 		pwk := findSector(stores.SectorName(s.sector), sealtasks.TTPreCommit1)
 		log.Infof("xjrw %v task = %s  pwk = %s hostname = %s", s.sector, task, pwk, inf.Hostname)
 		if pwk == "" {
 			return false, xerrors.Errorf("%v not exist", s.sector)
 		}
-		if pwk != inf.Hostname {
-			log.Infof("%v not in same server %v  %v", s.sector, pwk, inf.Hostname)
-			return false, nil
+
+		if exist {
+			if groupState[pwk].GroupName != group.GroupName {
+				log.Infof("%v not in group %v  %v  %v  %v", s.sector, groupState[pwk].GroupName, pwk, inf.Hostname, group.GroupName)
+				return false, nil
+			}
+			if index := int(s.sector.Number) % getGroupCount(group.GroupName); index != group.GroupIndex {
+				log.Infof("%v index %v  is different %v  %v", s.sector, index, inf.Hostname, group)
+				return false, nil
+			}
+		} else {
+			if pwk != inf.Hostname {
+				log.Infof("%v not in same server %v  %v", s.sector, pwk, inf.Hostname)
+				return false, nil
+			}
 		}
 	}
+
 	paths, err := whnd.w.Paths(ctx)
 	if err != nil {
 		return false, xerrors.Errorf("getting worker paths: %w", err)
