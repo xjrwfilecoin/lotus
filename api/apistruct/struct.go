66f8b7a2694b734041840b14b42a98d73133e68a
bigocean <3408598@qq.com>
2020-11-07 17:41:22 +0800

    test

--- api/apistruct/struct.go
+++ api/apistruct/struct.go
@@ -363,6 +363,7 @@ type WorkerStruct struct {
 		Remove          func(ctx context.Context, sector abi.SectorID) error                                                                                                                                       `perm:"admin"`
 		MoveStorage     func(ctx context.Context, sector abi.SectorID, types stores.SectorFileType) error                                                                                                          `perm:"admin"`
 		StorageAddLocal func(ctx context.Context, path string) error                                                                                                                                               `perm:"admin"`
+		SetSectorState  func(ctx context.Context, sector abi.SectorNumber, state string)                                                                                                                           `perm:"admin"`
 
 		UnsealPiece func(context.Context, abi.SectorID, storiface.UnpaddedByteIndex, abi.UnpaddedPieceSize, abi.SealRandomness, cid.Cid) error `perm:"admin"`
 		ReadPiece   func(context.Context, io.Writer, abi.SectorID, storiface.UnpaddedByteIndex, abi.UnpaddedPieceSize) (bool, error)           `perm:"admin"`
@@ -1449,6 +1450,10 @@ func (w *WorkerStruct) MoveStorage(ctx context.Context, sector abi.SectorID, typ
 
 func (w *WorkerStruct) StorageAddLocal(ctx context.Context, path string) error {
 	return w.Internal.StorageAddLocal(ctx, path)
+}
+
+func (w *WorkerStruct) SetSectorState(ctx context.Context, sector abi.SectorNumber, state string) {
+	w.Internal.SetSectorState(ctx, sector, state)
 }
 
 func (w *WorkerStruct) UnsealPiece(ctx context.Context, id abi.SectorID, index storiface.UnpaddedByteIndex, size abi.UnpaddedPieceSize, randomness abi.SealRandomness, c cid.Cid) error {
