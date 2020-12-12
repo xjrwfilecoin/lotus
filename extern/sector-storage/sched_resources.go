8e7285237d21f899ba0dcf90d0914a29cee528a6
bigocean <3408598@qq.com>
2020-12-12 11:29:26 +0800

    modify mem

--- extern/sector-storage/sched_resources.go
+++ extern/sector-storage/sched_resources.go
@@ -162,13 +162,13 @@ func (a *activeResources) utilization(wr storiface.WorkerResources) float64 {
 func (wh *workerHandle) utilization() float64 {
 	wh.lk.Lock()
 	u := wh.active.utilization(wh.info.Resources)
-	u += wh.preparing.utilization(wh.info.Resources)
+	//u += wh.preparing.utilization(wh.info.Resources)
 	wh.lk.Unlock()
-	wh.wndLk.Lock()
-	for _, window := range wh.activeWindows {
-		u += window.allocated.utilization(wh.info.Resources)
-	}
-	wh.wndLk.Unlock()
+	//wh.wndLk.Lock()
+	//for _, window := range wh.activeWindows {
+	//	u += window.allocated.utilization(wh.info.Resources)
+	//}
+	//wh.wndLk.Unlock()
 
 	return u
 }
