5fcc7192124c301ecdcd68dda9c379371ae2f8cb
paulwang <12900985@qq.com>
2021-07-01 14:56:28 +0800

    lotus-bench

--- extern/sector-storage/ffiwrapper/sealer_cgo.go
+++ extern/sector-storage/ffiwrapper/sealer_cgo.go
@@ -654,7 +654,7 @@ func (sb *Sealer) SealCommit1(ctx context.Context, sector storage.SectorRef, tic
 	if err != nil {
 		return nil, xerrors.Errorf("acquire sector paths: %w", err)
 	}
-	defer done()
+	done()
 	output, err := ffi.SealCommitPhase1(
 		sector.ProofType,
 		cids.Sealed,
